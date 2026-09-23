8dda661bd1711606d375f333823bad9776e332e7
DIGITALPRO-INVITACIONES <vigorex45@gmail.com>
2026-09-22 20:41:56 -0400

    Add files via upload

--- configuracion.js
+++ configuracion.js
@@ -1,5 +1,4 @@
 window.BODA = {
-  // Pegar aquí el enlace público para RESPONDER al formulario de Google.
-  formulario: '',
+  formulario: 'https://docs.google.com/forms/d/e/1FAIpQLSd9ceZTztz8l_z2A1emF8bJ22e1iQ6xTlGZgd8yCPHCZsLLqQ/viewform',
   fecha: '2026-12-19T13:00:00-04:00'
 };
